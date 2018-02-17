bfaa1e041221641e8a4feb9642182d0e611d21e4
user1 <satymovaanna@gmail.com>
2018-02-17 15:18:28 +0300

    added 2nd line

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@
 ### Exercise-1: Create a Python 3 code which outputs "Hello!" to the screen
 ### -----------------------------------------------------------------------p
 print ('Hello!')
+print('sdfsdfsd')
